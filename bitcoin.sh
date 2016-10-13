43dd83fa7918c303021a2836ec7ab86fe16abd5e
George Angelopoulos <george@usermod.net>
2016-10-14 01:44:33 +0200

    re-implement bitcoin blocklet in bash
    just for fun

---
+++ bitcoin.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# TODO: fail gracefully if curl fails (network issues)
+
+bitcoinity_url="http://bitcoinity.org/markets/bitstamp/USD"
+api_url="https://www.bitstamp.net/api/ticker/"
+
+[ "$BLOCK_BUTTON" = "1" ] && xdg-open "$bitcoinity_url"
+
+curl -s "$api_url" | awk -v RS="," '
+/last/ {
+    gsub("\"", "")
+    print $2
+    print $2
+}
+'
