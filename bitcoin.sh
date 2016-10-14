006d1730de555c82df85a453ff355d04d91fc338
George Angelopoulos <george@usermod.net>
2016-10-14 03:27:37 +0200

    bitcoin.sh fail gracefully if curl fails

--- bitcoin.sh
+++ bitcoin.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-# TODO: fail gracefully if curl fails (network issues)
-
 bitcoinity_url="http://bitcoinity.org/markets/bitstamp/USD"
 api_url="https://www.bitstamp.net/api/ticker/"
 
 [ "$BLOCK_BUTTON" = "1" ] && xdg-open "$bitcoinity_url"
 
-curl -s "$api_url" | awk -v RS="," '
+out="$(curl -s "$api_url")"
+
+if [ $? -ne 0 ]; then
+    echo "error"
+    echo "error"
+    echo "#ff0000"
+    exit 0
+fi
+
+printf "%s" "$out" | awk -v RS="," '
 /last/ {
     gsub("\"", "")
     print "$"$2
